cae35b1b6ec2ebdbcc7fc0ccdf357240fc2e7e11
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-03-07 20:35:03 -0800

    Creating user_id cookie and check for errors

--- express_server.js
+++ express_server.js
@@ -2,25 +2,56 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
 app.use(cookieParser());
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs"); //Telling Express app to use EJS as its templating engine
 
 //Creating 6 character string
 const generateRandomString = () => {
-const shortURL = Math.random().toString(36).replace(/[^a-z]+/g, '').slice(0, 7);;
-return shortURL;
+  const shortURL = Math.random()
+    .toString(36)
+    .replace(/[^a-z]+/g, "")
+    .slice(0, 6);
+  return shortURL;
 };
 
 //Create short: long URL object
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
+
+//create userID object to store id, email, password
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+//check an email in users object
+const isEmailUnique = (newEmail) => {
+  let result = true;
+  for (let user in users) {
+    let oldEmail = users[user].email;
+    if (oldEmail === newEmail) {
+      return false;
+    }
+  } 
+  return result;
+};
+
+const getUserFromCookie = (req) => users[req.cookies["user_id"]];
 
 //Creating / rout output
 app.get("/", (req, res) => {
@@ -29,25 +60,46 @@ app.get("/", (req, res) => {
 
 //Creating /urls rout output
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  console.log("user id cookie", req.cookies["user_id"]);
+  const templateVars = { urls: urlDatabase, user: getUserFromCookie(req) };
   res.render("urls_index", templateVars);
 });
 
 //Creating /urls/new rout to create a new URL's
 app.get("/urls/new", (req, res) => {
-  const templateVars = {urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = { urls: urlDatabase, user: getUserFromCookie(req) };
   res.render("urls_new", templateVars);
 });
 
 // Register new user
 app.get("/register", (req, res) => {
-  const templateVars = {urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = { urls: urlDatabase, user: getUserFromCookie(req) };
   res.render("urls_register", templateVars);
+});
+
+//Passing new user id, username, password from server
+app.post("/register", (req, res) => {
+  const id = generateRandomString();
+  const email = req.body.email.trim();
+  const password = req.body.password.trim();
+  if (email === "" || password === "") {
+    return res.send("Status code: 400. Email or password is missing");
+  } else if (isEmailUnique(email)) {
+    users[id] = { id, email, password };
+    res.cookie("user_id", users[id].id);
+  } else {
+     return res.send('User exist');
+   }
+  res.redirect("/urls");
 });
 
 // Rendering information about a single URL
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    user: getUserFromCookie(req),
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -66,22 +118,21 @@ app.get("/hello", (req, res) => {
 //Creating cookies to /login
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  res.cookie('username', username);
+  res.cookie("username", username);
   res.redirect("/urls");
 });
 
 // Creating /logout and clear the cookies
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
-
 //Creating /urls rout using our randomURL and then redirecting to thatt random URL
 app.post("/urls", (req, res) => {
-  const randomURL = generateRandomString ();
+  const randomURL = generateRandomString();
   urlDatabase[randomURL] = req.body.longURL;
-  res.redirect(`/urls/${randomURL}`);    
+  res.redirect(`/urls/${randomURL}`);
 });
 
 //Edit POST /urls/:id
@@ -89,7 +140,7 @@ app.post("/urls/:id", (req, res) => {
   const urlId = req.params.id;
   const newURL = req.body.longURL;
   urlDatabase[urlId] = newURL;
-  res.redirect("/urls");  
+  res.redirect("/urls");
 });
 
 //Deleting the URL from /urls/:shortURL/ rout
@@ -97,7 +148,6 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
-
 
 //Startin up a server at port #
 app.listen(PORT, () => {
